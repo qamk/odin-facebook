6d4885eab5b7d1a2a8596cfc1589c80815e64e3d
qamk <qamking@gmail.com>
2021-09-24 01:26:30 +0100

    Improve friend request controller & page design

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -19,11 +19,11 @@ class FriendRequestsController < ApplicationController
 
   def update
     @friend_request = FriendRequest.find(params[:id])
-    if @friend_request.update(request_parms)
+    if @friend_request.update(request_params)
       redirect_to root_path, notice: 'Made a new friend :-)'
     else
       flash[:error] = 'Failed to respond to friend request'
-      redirect_back fallback_location: '/', allow_other_hosts: false
+      redirect_to feed_path
     end
   end
 
@@ -32,17 +32,29 @@ class FriendRequestsController < ApplicationController
   private
 
   def grab_data_for_tab
-    friend_request_ids =
-      if params[:sent] == 'is-active'
-        FriendRequest.sender_only(current_user.id).pluck(:receiver_id)
+    tab_params_hash = tab_params
+    friend_requests = # formerly friend_request_ids
+      if tab_params[:sent] == 'is-active'
+        FriendRequest.sender_only(current_user.id).includes(:receiver)
       else
-        FriendRequest.receiver_only(current_user.id).pluck(:sender_id)
+        FriendRequest.receiver_only(current_user.id).includes(:sender)
       end
-    @requests = query_user_ids(friend_request_ids)
+    # @requests = query_user_ids(friend_request_ids)
+    users = associated_user_objects(collection: friend_requests, method: tab_params_hash[:method])
+    @requests = friend_requests.zip(users)
+  end
+
+  def associated_user_objects(collection:, method:)
+    method = method&.to_sym || :sender
+    collection.map { |item| item.send(method) } # metaprogramming to send user objects using the right method
   end
 
   def request_params
     params.require(:friend_request).permit(:sender_id, :receiver_id, :accepted)
+  end
+
+  def tab_params
+    params.permit(:sent, :method)
   end
 
 end
